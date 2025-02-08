49b1754606545432af40d47ade9cc126c6e19c24
Jenny <dolientrang@gmail.com>
2025-02-08 00:07:55 -0500

    google maps api

---
+++ src/app/pages/index.js
@@ -0,0 +1,10 @@
+import GoogleMapComponent from "@/components/GoogleMap";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Eco-Friendly Hotels & Restaurants</h1>
+      <GoogleMapComponent />
+    </div>
+  );
+}
